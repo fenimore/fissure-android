d4e0ac427128b2797cc92b5240c5e9445c886539
polypmer <exorable.ludos@gmail.com>
2016-08-01 19:27:20 -0400

    add clear button

--- app/src/main/java/com/workingagenda/fissure/MainActivity.java
+++ app/src/main/java/com/workingagenda/fissure/MainActivity.java
@@ -124,8 +124,12 @@ public class MainActivity extends AppCompatActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
         //noinspection SimplifiableIfStatement
+        if (id == R.id.action_clear) {
+            bitmaps.clear();
+            images.clear();
+            uris.clear();
+        }
         if (id == R.id.action_settings) {
             return true;
         }
