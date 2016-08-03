88166cdef36a73a270d0c7ba7db1ef176d3ad759
polypmer <exorable.ludos@gmail.com>
2016-08-03 11:44:11 -0400

    added activity view menu

--- app/src/main/java/com/workingagenda/fissure/ViewActivity.java
+++ app/src/main/java/com/workingagenda/fissure/ViewActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 /**
  * Created by fen on 8/3/16.
@@ -19,7 +21,26 @@ public class ViewActivity  extends AppCompatActivity {
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_view, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_load) {
+            // load gif
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         switch (requestCode) {
             case 0:
@@ -29,6 +50,10 @@ public class ViewActivity  extends AppCompatActivity {
                 break;
         }
         super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void loadGIF() {
+
     }
 
 }
