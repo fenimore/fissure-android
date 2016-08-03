3e5e8279c442171bdf7bbc80768f0d8f3ba3b3fa
polypmer <exorable.ludos@gmail.com>
2016-08-03 16:13:34 -0400

    continue polish

--- app/src/main/java/com/workingagenda/fissure/MainActivity.java
+++ app/src/main/java/com/workingagenda/fissure/MainActivity.java
@@ -106,11 +106,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_clear) {
             ClearAll();
             return true;
@@ -161,6 +157,7 @@ public class MainActivity extends AppCompatActivity {
                     Log.d("tmpFile", tmpFile.getPath());
                     tmpFile.delete();
                     bitmaps.add(bitmap);
+                    // TODO: FILE name?
                     images.add(uri.getPath());
                     uris.add(uri);
                     ((ArrayAdapter) lv.getAdapter()).notifyDataSetChanged();
