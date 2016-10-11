f61650911f8c8ecd8c0b9ad7f6a5fde98186aff5
polypmer <exorable.ludos@gmail.com>
2016-10-11 17:23:04 -0400

    Reimplement select button

--- app/src/main/java/com/workingagenda/fissure/ViewActivity.java
+++ app/src/main/java/com/workingagenda/fissure/ViewActivity.java
@@ -17,8 +17,10 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.webkit.WebView;
 
+import android.widget.Button;
 import android.widget.Toast;
 
 import java.io.File;
@@ -39,7 +41,7 @@ public class ViewActivity  extends AppCompatActivity {
     private Uri uri;
     final int chunkSize = 1024; // One kb at a time
     private byte[] imageData = new byte[chunkSize];
-
+    private Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -53,6 +55,19 @@ public class ViewActivity  extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        btn = (Button) findViewById(R.id.load_btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(), FilesActivity.class);
+                startActivityForResult(intent, 0);
+            }
+        });
+        if (tmpFile == null) {
+            btn.setVisibility(View.VISIBLE);
+        } else {
+            btn.setVisibility(View.GONE);
+        }
     }
 
     @Override
@@ -197,6 +212,7 @@ public class ViewActivity  extends AppCompatActivity {
             webView.clearCache(true); // For changing the view, literally
             webView.loadDataWithBaseURL("file://android_asset/", result, "text/html", "utf-8", null);
             // tmpFile deletes onDestroy()
+            btn.setVisibility(View.GONE);
 
         }
     }
